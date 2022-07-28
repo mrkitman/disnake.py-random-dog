2e047719d40a410d18c898859ddb61a93a17ddee
unknown <mrkitman@mail.ru>
2022-07-28 12:19:40 +0300

    remove space

--- dog.py
+++ dog.py
@@ -1,1 +1,10 @@
-git add README.md
+import aiohttp
+
+@bot.slash_command (description='Рандомная картинка собаки')
+async def dog(self, inter):
+    async with aiohttp.ClientSession () as cs:
+        async with cs.get ( "https://random.dog/woof.json" ) as r:
+            data = await r.json ()
+            embed = disnake.Embed (title="Собака барабака",colour=0x2f3136)
+            embed.set_image ( url=data['url'] )
+            await inter.send ( embed=embed )
